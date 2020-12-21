4cfcfd509dd138ee6ed0c3fcfcfeb16292d19e9c
Jiwon1300 <63456995+Jiwon1300@users.noreply.github.com>
2020-12-21 17:04:56 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const {token,prefix,rbx_cookie} = require('./settings.json');
+const client = new Discord.Client();
+const fs = require('fs');
+const RobloxModule = require('./Commands/module/RobloxModule')
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith(".js"));
+for(const file of commandFiles){
+    const command = require(`./Commands/${file}`);
+    client.commands.set(command.name,command);
+}
+client.once('ready',()=>{
+    console.log("Ready");
+    RobloxModule.login(rbx_cookie)
+});
+
+client.on('message',(message)=>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift();
+    if(!client.commands.has(command)) return;
+    try{
+        client.commands.get(command).execute(message,args);
+    }catch(error){
+        console.log(error)
+    }
+})
+
+client.login(token)
