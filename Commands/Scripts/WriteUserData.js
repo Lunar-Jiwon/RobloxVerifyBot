a9660fc8cd0f6f03523aef8939fde03ac77c50a8
Jiwon1300 <63456995+Jiwon1300@users.noreply.github.com>
2020-12-29 11:24:35 +0900

    재인증 기능에 사용 될 업데이트 데이터 모듈 추가

--- Commands/Scripts/WriteUserData.js
+++ Commands/Scripts/WriteUserData.js
@@ -1,6 +1,6 @@
 const googleSpreadsheet = require('google-spreadsheet');
 const creds = require('../../Data/skey.json');
-const doc = new googleSpreadsheet("스프레드시트아이디");
+const doc = new googleSpreadsheet("스프레드시트 아이디");
 module.exports.WriteData = function(DiscordId,RobloxId){
     const data = {DiscordId: DiscordId, RobloxId: RobloxId};
     doc.useServiceAccountAuth(creds,function(err){
@@ -9,6 +9,38 @@ module.exports.WriteData = function(DiscordId,RobloxId){
             data,
             function(err){
                 console.log(err);
+            }
+        )
+    });
+}
+
+module.exports.UpdateData = function(DiscordId,Robloxid){
+    const data = {DiscordId: DiscordId, RobloxId: Robloxid};
+    doc.useServiceAccountAuth(creds,function(err){
+        doc.getCells(
+            1,
+            {
+                "max-row":1000,
+                "max-col": 2,
+                "reverse": true,
+    
+            },function(err,cells){
+                console.log(err)
+                for(var num = 0; cells.length > num; num++){
+                    if(cells[num].value == DiscordId){
+                        var un = num + 1
+                        cells[un].value = Robloxid;
+                        return cells[un].save();
+                    }
+                }
+            
+                doc.addRow(
+                    1,
+                    data,
+                    function(err){
+                        console.log(err);
+                    }
+                )
             }
         )
     });
