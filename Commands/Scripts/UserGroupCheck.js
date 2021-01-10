6a3e09cfc824add16f6b2751a204c3483c348e55
Jiwon1300 <63456995+Jiwon1300@users.noreply.github.com>
2021-01-10 15:30:14 +0900

    부 그룹이 있을 때 인증 시 메시지가 두 번 나오는 오류 수정

--- Commands/Scripts/UserGroupCheck.js
+++ Commands/Scripts/UserGroupCheck.js
@@ -1,6 +1,7 @@
 const roblox = require('noblox.js');
 const fs = require('fs');
 const embed = require('../module/DiscordModule');
+var isjoin = null
 module.exports.CheckUserGroup = function(message,robloxuserid){
     try{
         const data = fs.readFileSync('./Data/ServerData.json','utf8');
@@ -30,50 +31,60 @@ module.exports.CheckUserGroup = function(message,robloxuserid){
 
             for(var i = 0; i < Object.keys(serverdatas[message.guild.id].Ranks).length; i++){
                 var theTypeIs = Object.keys(serverdatas[message.guild.id].Ranks)[i];
-                
-                
-    
                 if(rank == theTypeIs){
                     var rolename = serverdatas[message.guild.id].Ranks[theTypeIs].split(",");
                     for(const role of rolename){
                         message.member.roles.add(role)
                     }
+                    
                     roblox.getRankNameInGroup(serverdatas[message.guild.id].DefaultGroup,robloxuserid).then((rankname)=>{
+                        
                         if(serverdatas[message.guild.id].SubGroup != null){
                             for(var x = 0; x < Object.keys(serverdatas[message.guild.id].SubGroup).length; x++){
                                 var rs = Object.keys(serverdatas[message.guild.id].SubGroup)[x];
-                                roblox.getRankNameInGroup(rs,robloxuserid).then((rank)=>{
-                                    if(rank != "Guest"){
-                                        console.log("sd")
-                                        var name = rankname.substring(rankname.indexOf("["),rankname.lastIndexOf("]")+1);
-                                        console.log(rs)
+                                
+                                roblox.getRankNameInGroup(rs,robloxuserid).then((rank1)=>{
+                                    if(rank1 == "Guest") return isjoin = false
+                                    ifjoin = true
+                                    var name = rankname.substring(rankname.indexOf("["),rankname.lastIndexOf("]")+1);
                                         roblox.getUsernameFromId(robloxuserid).then((username)=>{
                                             message.member.setNickname(`${name} (${serverdatas[message.guild.id].SubGroup[rs].Nickname}) ${username}`)
                                             message.member.roles.add(serverdatas[message.guild.id].SubGroup[rs].RoleId)
                                             return message.channel.send(embed.SuccessfulEmbed("인증 완료","성공적으로 인증이 완료되었습니다.\n역할 지급은 여러번 인증 시 딜레이가 발생할 수 있습니다."));
                                         })
-                                    }
                                     return
                                 })
                                 
                             }
-                        }
-                        console.log(rankname)
+                            if (!isjoin && isjoin != null){
+                                roblox.getRankNameInGroup(serverdatas[message.guild.id].DefaultGroup,robloxuserid).then((rankname)=>{
+                                        var name = rankname.substring(rankname.indexOf("["),rankname.lastIndexOf("]")+1);
+                                        roblox.getUsernameFromId(robloxuserid).then((username)=>{
+                                            message.member.setNickname(`${name} ${username}`)
+                                            return message.channel.send(embed.SuccessfulEmbed("인증 완료","성공적으로 인증이 완료되었습니다.\n역할 지급은 여러번 인증 시 딜레이가 발생할 수 있습니다."));
+                                        })
+                                })
+                            }
+                            
+                            
+                        }else{
                             var name = rankname.substring(rankname.indexOf("["),rankname.lastIndexOf("]")+1);
                             roblox.getUsernameFromId(robloxuserid).then((username)=>{
-                            message.member.setNickname(`${name} ${username}`)
-                            return message.channel.send(embed.SuccessfulEmbed("인증 완료","성공적으로 인증이 완료되었습니다.\n역할 지급은 여러번 인증 시 딜레이가 발생할 수 있습니다."));
-                        })
+                                message.member.setNickname(`${name} ${username}`)
+                                return message.channel.send(embed.SuccessfulEmbed("인증 완료","성공적으로 인증이 완료되었습니다.\n역할 지급은 여러번 인증 시 딜레이가 발생할 수 있습니다."));
+                            })
+                        }
+                        
                         
                     })
                 
                 }
             }
-            
         }).catch((error)=>{
             message.delete();
             message.channel.send(embed.ErrorEmbed("인증",`오류 : ${error}`))
         })
+        
     }catch(error){
         console.log(error)
     }
